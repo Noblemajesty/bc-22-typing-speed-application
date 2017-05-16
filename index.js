99a3de9899c7c74f724d80765f8c3ad2df26651d
Fred Adewole <Fred Adewole>
2017-05-16 06:39:29 -0700

    Add package.json file and index.js containing route and working Authentication feature

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+let app = express();
+/*
+let logger = (req, res, next) => {
+	console.log('Logging...');
+	next();
+}
+
+app.use(logger);*/
+
+// Set view Engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Set static Path
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get ('/', (req,res) => {
+	res.render('index', {
+		name: "Fred Adewole"
+	});
+})
+
+// Set server port
+app.listen(3000, e => {
+	console.log('Server running on port 3000...');
+})
