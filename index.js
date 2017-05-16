e10003b07efa1b455c6ad384ccdd4c0a7bbfa5c7
Fred Adewole <Fred Adewole>
2017-05-16 07:49:08 -0700

    Update Application

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get ('/', (req,res) => {
-	res.render('index', {
+	res.render('home', {
 		name: "Fred Adewole"
 	});
 })
